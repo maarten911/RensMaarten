88705dedef17e588a9d7ac1feb4339bf931bbd08
maarten-laptop <mjgvanhooft@gmail.com>
2020-05-24 17:07:25 +0200

    Add std deviations and bull market filter

--- backtest.py
+++ backtest.py
@@ -7,31 +7,49 @@ import matplotlib.pyplot as plt
 def add_features(df):
     # Create features
     # df["o_scaled_50"] = df["o"].rolling(window=50, min_periods=50, axis=0).apply(lambda x: (x[-1] - np.min(x)) / (np.max(x) - np.min(x)), raw=True)
-    df["rsi_s"] = features.get_rsi(df, period=30)
-    df["rsi_f"] = features.get_rsi(df, period=5)
+    # df["rsi_s"] = features.get_rsi(df, period=30)
+    df["rsi_f"] = features.get_rsi(df, period=14)
     df["stoch_rsi"] = features.get_stoch_rsi(df["c"], period=14)
     df["stoch"] = features.get_stoch(df, period=14)
 
     # We don't have to perform shifting, because we use the open price
-    df["std_short"] = df["o"].rolling(20).std()
-    df["std_long"] = df["o"].rolling(200).std()
-    df["std_factor"] = df["std_short"]/df["std_long"]
+    n_long = 2000
+    df["std_short"] = df["o"].rolling(100).std()
+    df["std_long"] = df["o"].rolling(n_long).std()
+    mean_factor = df["std_long"].mean()/df["std_short"].mean()
+    df["std_factor"] = mean_factor*df["std_short"]/df["std_long"]
+    df["std_factor"] = df["std_factor"].apply(lambda x: 0.5 if x < 0.5 else x)
+    df["std_factor"] = df["std_factor"].apply(lambda x: 1.5 if x > 1.5 else x)
+    df["ma_fast"] =df["o"].rolling(10).mean()
+    df["ma_slow"] =df["o"].rolling(40).mean()
+
+    # min_std = df["std_factor"].min()
+    # max_std = df["std_factor"].max()
+    # mean_std = df["std_factor"].mean()
+    #
+    # # Standardize to desired interval
+    # a = 0.5
+    # b = 1.5
+    # df["std_factor"]= df["std_factor"].apply(lambda x: (x - min_std)/(max_std - min_std))
+    # df["std_factor"] = df["std_factor"].apply(lambda x: a + (b - a)*x)
+    # df["std_factor"] = df["std_factor"].apply(lambda x: b if x > b else x)
+    # df["std_factor"] = df["std_factor"].apply(lambda x: a if x < b else x)
 
     # Drop nans at beginning and end
     first_ix = df.first_valid_index()
     last_ix = df.last_valid_index()
-    df = df.loc[first_ix:last_ix]
+    df = df.loc[n_long:last_ix]
 
     return df
 
 
 def add_entry_signal(row):
     rsi_threshold = 40
-    stoch_rsi_threshold = 25
-    stoch_threshold = 35
+    stoch_rsi_threshold = 15
+    stoch_threshold = 30
 
     # If oversold:
-    if (row["rsi_f"] < rsi_threshold) and (row["rsi_s"] < rsi_threshold)and (row["stoch_rsi"] < stoch_rsi_threshold) and (row["stoch"] < stoch_threshold):
+    if (row["rsi_f"] < rsi_threshold) and (row["stoch_rsi"] < stoch_rsi_threshold) and (row["stoch"] < stoch_threshold) and (row["ma_fast"] > row["ma_slow"]):
         return 1
     # Else
     else:
@@ -70,12 +88,13 @@ def perform_backtest(df, market):
     for ix in df.index:
         if position == 0:
             if df.loc[ix]["entry"] == 1:
+                std = df.loc[ix]["std_factor"]
                 position = 1
                 open_price = df.loc[ix]["o"]
                 print(f"Open long: {df.loc[ix]['d']} at {open_price}")
 
                 # Check if we can close already (we open at opening bar,so we have a h/l to process)
-                profit_update, close_price = check_exit(df.loc[ix], open_price, take_profit_pct, stop_loss_pct, spread_pct)
+                profit_update, close_price = check_exit(df.loc[ix], open_price, take_profit_pct*std, stop_loss_pct*std, spread_pct)
                 if profit_update != 0:
                     profit += profit_update
                     trade_dates += [df.loc[ix]["d"]]
@@ -85,7 +104,7 @@ def perform_backtest(df, market):
         # If we are long
         elif position > 0:
             # Check if we can close already (we open at opening bar,so we have a h/l to process)
-            profit_update, close_price = check_exit(df.loc[ix], open_price, take_profit_pct, stop_loss_pct, spread_pct)
+            profit_update, close_price = check_exit(df.loc[ix], open_price, take_profit_pct*std, stop_loss_pct*std, spread_pct)
             if profit_update != 0:
                 profit += profit_update
                 position = 0
@@ -96,18 +115,26 @@ def perform_backtest(df, market):
         elif position < 0:
             print("Not implemented")
 
+    plt.figure()
     plt.plot(trade_dates, profit_list)
     plt.grid()
     plt.savefig(f"{market}.png")
     plt.xticks(rotation=90)
 
 
-markets = ["BTCUSDT"]
-
+markets = ["BTCUSDT",
+           #  "NANOUSDT",
+           # "NEOUSDT",
+           # "XMRUSDT",
+           # "XRPUSDT",
+           #  "BTCUSDT",
+           # "ETHUSDT"]
+           ]
 for market in markets:
     # Read data and add features
     df = pd.read_csv(f"{market}.csv")
-    df = df.sort_values("d")
+    df = df.drop_duplicates().reset_index(drop=True)
+    df = df.sort_values("d").reset_index(drop=True)
     df = add_features(df)
     df["d"] = pd.to_datetime(df["d"])
     df["entry"] = df.apply(add_entry_signal, axis=1).shift(1)
--- get_historical_data.py
+++ get_historical_data.py
@@ -6,12 +6,13 @@ pd.set_option("display.max_columns", 99)
 
 markets = ['BTC',
            "ETH",
-           # "NEO",
-           # "XRP",
-           # "XMR",
-           # "DASH",
-           # "NANO",
-           "BCH"]
+           "NEO",
+           "XRP",
+           "XMR",
+           "DASH",
+           "NANO",
+           "BCH",
+           ]
 markets = [m + "USDT" for m in markets]
 tick_interval = '30m'
 market_to_df = {}
@@ -20,7 +21,7 @@ for market in markets:
     print(f"Getting data for {market}\n")
     end_time = int(time.time() * 1000)
     df_list = []
-    for i in range(5):
+    for i in range(50):
         # Update dates
         start_time = int((end_time / 1000 - 30 * 500 * 60) * 1000)
         print(datetime.utcfromtimestamp(start_time / 1000), datetime.utcfromtimestamp(end_time / 1000))
@@ -34,11 +35,16 @@ for market in markets:
                                 3:"l",
                                 4:"c",
                                 5:"v"})
-        df = df[["d","o","h","l","c","v"]]
-        df["d"] = df["d"].apply(lambda x: datetime.utcfromtimestamp(x/1000).strftime('%Y-%m-%d %H:%M:%S'))
-        df_list += [df]
 
-        end_time = start_time
+        try:
+            df = df[["d","o","h","l","c","v"]]
+            df["d"] = df["d"].apply(lambda x: datetime.utcfromtimestamp(x/1000).strftime('%Y-%m-%d %H:%M:%S'))
+            df_list += [df]
+
+            end_time = start_time
+        except:
+            pass
+
     # Process
     df = pd.concat(df_list, axis=0).reset_index(drop=True)
     df.sort_values("d")
